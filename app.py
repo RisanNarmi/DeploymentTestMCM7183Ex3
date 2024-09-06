6ba62c709a863b6e4eadcfb5c8e074e4f08a05f3
RisanNarmi <qwertzer8@gmail.com>
2024-09-06 20:36:24 +0800

    Update app.py
    Graph Add

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from dash import Dash, html
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import plotly.express as px
 
 app = Dash(__name__)
 server = app.server
 
-app.layout = [html.H3('Hello world! Hope you have a terrible day')]
+app.layout = [html.H1('Hello, look at this graph')]
+df = pd.read_csv("https://raw.githubusercontent.com/RisanNarmi/DeploymentTestMCM7183Ex3/main/gdp_1960_2020.csv")
+subMY = df[df["country"].isin(["Malaysia"])]
+fig = px.scatter(subMY, x="year", y="gdp")
 
 if __name__ == '__main__':
     app.run(debug=True)
