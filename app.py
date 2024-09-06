98cbd30840257147647891d8c499f9ef48767b42
RisanNarmi <qwertzer8@gmail.com>
2024-09-06 20:39:20 +0800

    Update app.py
    added Dcc

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import plotly.express as px
