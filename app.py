1f8937e6904bf5a4423a3d4c444f4fe693018abb
RisanNarmi <qwertzer8@gmail.com>
2024-09-06 19:11:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from dash import Dash, html
 app = Dash(__name__)
 server = app.server
 
-app.layout = [html.H1('Hello world')]
+app.layout = [html.H3('Hello world! Hope you have a terrible day')]
 
 if __name__ == '__main__':
     app.run(debug=True)
