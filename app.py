4fe76b264557c0a26463657a286034aa5f809d5c
RisanNarmi <qwertzer8@gmail.com>
2024-09-06 20:51:08 +0800

    Update app.py
    Added layout

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ import plotly.express as px
 app = Dash(__name__)
 server = app.server
 
-app.layout = [html.H1('Hello, look at this graph')]
 df = pd.read_csv("https://raw.githubusercontent.com/RisanNarmi/DeploymentTestMCM7183Ex3/main/gdp_1960_2020.csv")
 subMY = df[df["country"].isin(["Malaysia"])]
 fig = px.scatter(subMY, x="year", y="gdp")
+
+app.layout = [html.H1('Hello, look at this graph'), dcc.graph(figure=fig)]
 
 if __name__ == '__main__':
     app.run(debug=True)
