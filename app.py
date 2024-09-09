3d7d628e9a109fd9c87c26ca914160c25810c163
RisanNarmi <qwertzer8@gmail.com>
2024-09-09 18:20:51 +0800

    Update app.py
    Added pie

--- app.py
+++ app.py
@@ -8,9 +8,19 @@ server = app.server
 
 df = pd.read_csv("https://raw.githubusercontent.com/RisanNarmi/DeploymentTestMCM7183Ex3/main/gdp_1960_2020.csv")
 subMY = df[df["country"].isin(["Malaysia"])]
+sub2020 = df[df["year"].isin([2020])]
+subASIA_2020 = sub2020[sub2020['state'].isin(['Asia'])]
+subEU_2020 = sub2020[sub2020['state'].isin(['Europe'])]
+subOCE_2020 = sub2020[sub2020['state'].isin(['Oceania'])]
+subAMERICAS_2020 = sub2020[sub2020['state'].isin(['America'])]
+subAFRICA_2020 = sub2020[sub2020['state'].isin(['Africa'])]
+pie_data = sum(subASIA_2020["gdp"]), sum(subEU_2020["gdp"]), sum(subOCE_2020["gdp"]), sum(subAMERICAS_2020["gdp"]), sum(subAFRICA_2020["gdp"])
+pie_df = {"continent":Chart_Lable,
+         "gdp":pie_data}
 fig = px.scatter(subMY, x="year", y="gdp")
+fig2 = px.pie(pie_df, values="gdp", names="continent")
 
-app.layout = [html.H1('Hello, look at this graph'), dcc.Graph(figure=fig)]
+app.layout = [html.H1('Hello, look at this graph'), dcc.Graph(figure=fig), dcc.Graph(figure=fig2)]
 
 if __name__ == '__main__':
     app.run(debug=True)
